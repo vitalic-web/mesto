8f9906b03bdfe80af468316458fa26266ece63f4
Vitalic-web <vitalic.web@gmail.com>
2020-05-14 02:09:12 +0200

    проектная работа 4.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,23 @@
-// создаем переменные для открытия и закрытия попапа
+// создаем переменные
 const profileEditButton = document.querySelector('.profile__title-button');
 const popupCloseButton = document.querySelector('.popup__close-icon');
-
-// создаем переменные для отображения на странице и полей форм
-let nameInput = document.querySelector('.popup__input_name');
-let profInput = document.querySelector('.popup__input_prof');
-let nameOutput = document.querySelector('.profile__title-name');
-let profOutput = document.querySelector('.profile__subtitle');
-
-
-//создаем переменную попапа
-//добавляем класс отображения попапа
-let visiblePopup = document.querySelector('.popup');
-visiblePopup.classList.add('popup__active');
+const nameInput = document.querySelector('.popup__input_name');
+const profInput = document.querySelector('.popup__input_prof');
+const nameOutput = document.querySelector('.profile__title-name');
+const profOutput = document.querySelector('.profile__subtitle');
+const visiblePopup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__container');
 
 //создаем функцию на открытие/закрытие попап
 //так же условие if добаляет текст со страницы
 function openPopup() {
-  visiblePopup.classList.toggle('popup');
+  visiblePopup.classList.toggle('popup_active');
 
-  if (visiblePopup.classList.contains('popup__active')) {
+  if (visiblePopup.classList.contains('popup_active')) {
     nameInput.value = nameOutput.textContent;
     profInput.value = profOutput.textContent;
   }
 }
-
-// добавляем событие по клику на открытие/закрытие попапа
-profileEditButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", openPopup);
-
-
-// создаем переменную для внесения изменений в попап
-let popupForm = document.querySelector('.popup__container');
 
 // создаем функцию для отправки формы на страницу
 // добавляем функцию openPopup для закрытия окна псоле ввода данных
@@ -44,8 +30,13 @@ function formSubmitHandler(evt) {
   openPopup();
 }
 
+// добавляем событие по клику на открытие/закрытие попапа
+profileEditButton.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", openPopup);
+
 // добавляем событие при клике на "сохранить"
 popupForm.addEventListener('submit', formSubmitHandler);
+
 
 
 
